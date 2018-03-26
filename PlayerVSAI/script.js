4da53f00d267b09dd4e39bec4c6e4f8b14bf45a1
JamesHannigan <jh861@kent.ac.uk>
2018-03-26 14:38:57 +0100

    Mainly comments added, which some code changes

--- PlayerVSAI/script.js
+++ PlayerVSAI/script.js
@@ -1,26 +1,28 @@
-var original;
-const huPlayer = 'X';
-const aiPlayer = 'O';
-const winCombos = [
+var currentState;
+var player1 = 'X';
+var computer = 'O';
+var playerWins = 0;
+var computerWins = 0;
+var drawsScore = 0;
+var winCombos = [
+    //Defines horizontal wins
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
+    //Defines vertical wins
 	[0, 3, 6],
 	[1, 4, 7],
 	[2, 5, 8],
+    //Defines diagonal wins
 	[0, 4, 8],
 	[6, 4, 2]
 ]
-var huWins = 0;
-var aiWins = 0;
-var tiesNo = 0;
 
 var boardData = document.querySelectorAll('.cell');
-constructorFunc();
 
 function constructorFunc() {
 	document.querySelector(".scoreCard").style.display = "none";
-	original = Array.from(Array(9).keys());
+	currentState = Array.from(Array(9).keys());
 	for (var i = 0; i < boardData.length; i++) {
 		boardData[i].innerText = '';
 		boardData[i].addEventListener('click', turnClick, false);
@@ -28,14 +30,14 @@ function constructorFunc() {
 }
 
 function turnClick(square) {
-	if (typeof original[square.target.id] == 'number') {
-		turn(square.target.id, huPlayer)
-		if (!checkWin(original, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+	if (typeof currentState[square.target.id] == 'number') {
+		turn(square.target.id, player1)
+		if (!checkWin(currentState, player1) && !checkTie()) turn(bestSpot(), computer);
 	}
 }
 
 function turn(squareId, player) {
-	original[squareId] = player;
+	currentState[squareId] = player;
 	document.getElementById(squareId).innerText = player;
     if(player == 'X'){
             document.getElementById(squareId).style.color = '#8dd14d'; //Was #ee5a3b
@@ -43,78 +45,20 @@ function turn(squareId, player) {
         else if(player == 'O'){
             document.getElementById(squareId).style.color = '#f3823d';
         }
-	let gameWon = checkWin(original, player)
+	let gameWon = checkWin(currentState, player)
 	if (gameWon) gameOver(gameWon)
 }
 
-function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) =>
-		(e === player) ? a.concat(i) : a, []);
-	let gameWon = null;
-	for (let [index, win] of winCombos.entries()) {
-		if (win.every(elem => plays.indexOf(elem) > -1)) {
-			gameWon = {index: index, player: player};
-			break;
-		}
-	}
-	return gameWon;
-}
-
-function updateScores(){
-        document.getElementById("scores").innerHTML= "Scores: Player: " + huWins + " Draws: " + tiesNo + " Computer: " + aiWins;}
-
-function gameOver(gameWon) {
-	for (let index of winCombos[gameWon.index]) {
-		//document.getElementById(index).style.backgroundColor =
-			//gameWon.player == huPlayer ? "blue" : "red";
-	}
-	for (var i = 0; i < boardData.length; i++) {
-		boardData[i].removeEventListener('click', turnClick, false);
-	}
-	declareWinner(gameWon.player == huPlayer ? "Player wins!" : "Computer wins.");
-    console.log("Player win- " +gameWon.player);
-    if(gameWon.player == 'X'){
-        aiWins++;
-    }
-    else if(gameWon.player = 'O'){
-        huWins++;
-    }
-    updateScores();
-}
-
-function declareWinner(who) {
-	document.querySelector(".scoreCard").style.display = "block";
-	document.querySelector(".scoreCard .text").innerText = who;
-}
-
-function emptySquares() {
-	return original.filter(s => typeof s == 'number');
-}
-
 function bestSpot() {
-	return minimax(original, aiPlayer).index;
-}
-
-function checkTie() {
-	if (emptySquares().length == 0) {
-		for (var i = 0; i < boardData.length; i++) {
-			//boardData[i].style.backgroundColor = "green";
-			boardData[i].removeEventListener('click', turnClick, false);
-		}
-		declareWinner("Tie Game!")
-        tiesNo++;
-        updateScores();
-		return true;
-	}
-	return false;
+	return minimax(currentState, computer).index;
 }
 
 function minimax(newBoard, player) {
 	var availSpots = emptySquares();
 
-	if (checkWin(newBoard, huPlayer)) {
+	if (checkWin(newBoard, player1)) {
 		return {score: -10};
-	} else if (checkWin(newBoard, aiPlayer)) {
+	} else if (checkWin(newBoard, computer)) {
 		return {score: 10};
 	} else if (availSpots.length === 0) {
 		return {score: 0};
@@ -125,11 +69,11 @@ function minimax(newBoard, player) {
 		move.index = newBoard[availSpots[i]];
 		newBoard[availSpots[i]] = player;
 
-		if (player == aiPlayer) {
-			var result = minimax(newBoard, huPlayer);
+		if (player == computer) {
+			var result = minimax(newBoard, player1);
 			move.score = result.score;
 		} else {
-			var result = minimax(newBoard, aiPlayer);
+			var result = minimax(newBoard, computer);
 			move.score = result.score;
 		}
 
@@ -139,7 +83,7 @@ function minimax(newBoard, player) {
 	}
 
 	var bestMove;
-	if(player === aiPlayer) {
+	if(player === computer) {
 		var bestScore = -10000;
 		for(var i = 0; i < moves.length; i++) {
 			if (moves[i].score > bestScore) {
@@ -158,4 +102,58 @@ function minimax(newBoard, player) {
 	}
 
 	return moves[bestMove];
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < boardData.length; i++) {
+			boardData[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+        drawsScore++;
+        updateScores();
+		return true;
+	}
+	return false;
+}
+
+function declareWinner(who) {
+	document.querySelector(".scoreCard").style.display = "block";
+	document.querySelector(".scoreCard .text").innerText = who;
+}
+
+function updateScores(){
+        document.getElementById("scores").innerHTML= "Scores: Player: " + playerWins + " Draws: " + drawsScore + " Computer: " + computerWins;}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+	for (var i = 0; i < boardData.length; i++) {
+		boardData[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == player1 ? "Player wins!" : "Computer wins.");
+    if(gameWon.player == 'X'){
+        computerWins++;
+    }
+    else if(gameWon.player = 'O'){
+        playerWins++;
+    }
+    updateScores();
+}
+}
+
+function emptySquares() {
+	return currentState.filter(s => typeof s == 'number');
 }
--- PlayerVSPlayer/script.js
+++ PlayerVSPlayer/script.js
@@ -1,88 +1,86 @@
-// SCRIPT FOR THE GAME //
-    var player1 = {name:"Player 1", value:"X", score:0};
-    var player2 = {name:"Player 2", value:"O", score:0};
-    var playerAI = {name:"Computer", value:"O", score:0};
-    var boardWidth = 19;
-    var playerType;
-    var currentPlayer;
-    var boardData;
-    var drawsScore = 0;
-  
-    function updateGameType(){
-        deleteRows();
-        //playerType = document.getElementById('playerChoice').options[document.getElementById('playerChoice').selectedIndex].value;
-        gameType = document.getElementById('gameChoice').options[document.getElementById('gameChoice').selectedIndex].value;
-        if(playerType == 1){
-            currentPlayer = playerAI;
-        }
-        if(gameType == 1){
-            boardWidth = 19;
-            document.getElementById('sampleTable').style.width = '90vw';
-            document.getElementById('sampleTable').style.height = '60vw';
-        }
-        else{
-            boardWidth = 3;
-            document.getElementById('sampleTable').style.width = '25vw';
-            document.getElementById('sampleTable').style.height = '25vw';
-            //document.getElementsByClassName('cells').style.fontSize = "25px";
-        }
-        constructorFunc();
+/**
+    This javascript file deals only with the functionality of the player vs player game.
+    Some of its major functions are: selecting a square, updating the array, making the table for each mode,
+    calculating the result, declaring the winner, recording the score and resetting the entire game.
+**/
+
+//The following 2 variables initalie the players involved in the game.
+var player1 = {
+    name: "Player 1",
+    value: "X",
+    score: 0
+};
+var player2 = {
+    name: "Player 2",
+    value: "O",
+    score: 0
+};
+//The following are global variables that determine players and properties of the board
+var boardWidth;
+var currentPlayer;
+var boardData;
+var drawsScore = 0;
+
+/**
+*   This is the constructor for the file. It completely resets and updates all components of the game.
+*   This function is also called if the player presses the reset button.
+**/
+function constructorFunc() {
+    deleteRows();
+    gameType = document.getElementById('gameChoice').options[document.getElementById('gameChoice').selectedIndex].value;
+    if (gameType == 1) {
+        boardWidth = 19;
+        document.getElementById('gameTable').style.width = '90vw';
+        document.getElementById('gameTable').style.height = '60vw';
+    } else {
+        boardWidth = 3;
+        document.getElementById('gameTable').style.width = '25vw';
+        document.getElementById('gameTable').style.height = '25vw';
     }
-    
-    function constructorFunc(){
-        if(playerType == 1){
-            togglePlayerAI();
-        }
-        else{
-            togglePlayer();
-        }
-        makeArray();
-        makeTable();
-        updateScore();
+    togglePlayer();
+    makeArray();
+    makeTable();
+    updateScore();
+}
+
+/**
+    This method makes a 2d array based on whatever the game mode is selected (19x19 or 3x3)
+**/
+function makeArray() {
+    boardData = new Array(boardWidth);
+    for (i = 0; i < boardWidth; i++) {
+        boardData[i] = new Array(boardWidth);
     }
-    
-    function deleteRows(){
-        var Parent = document.getElementById("sampleTable");
-            while(Parent.hasChildNodes()){
-               Parent.removeChild(Parent.firstChild);
-            }
-    }
-    
-    function makeArray(){
-        boardData = new Array(boardWidth);
-        for (i=0; i <boardWidth; i++){
-            boardData[i]=new Array(boardWidth);
+}
+
+/**
+    This method makes a table on the html determines on the size of the game mode (19x19 or 3x3).
+    It achieves this through for loop calls.
+**/
+function makeTable() {
+    var table = document.getElementById('gameTable');
+    for (j = boardWidth - 1; j >= 0; j--) {
+        var tr = document.createElement('tr');
+        var tableString = "";
+        for (i = 0; i < boardWidth; i++) {
+            var id = j + ',' + i;
+            tableString += '<td class="cells" id="td=' + id + '" x="' + j + '" y="' + i + '" onclick="select(this);"></td>';
         }
-    }
-    
-    function makeTable(){
-        var xTable=document.getElementById('sampleTable');
-        for(j=boardWidth-1; j>=0; j--){
-        var tr=document.createElement('tr');
-          var tableString = "";
-          for(i=0; i<boardWidth; i++){
-              var id = j+','+i;
-              tableString += '<td class="cells" id="td='+id+'" x="'+j+'" y="'+i+'" onclick="select(this);"></td>';
-          }
         tr.innerHTML = tableString;
-          xTable.appendChild(tr);
-        }
+        table.appendChild(tr);
     }
-    
-    function updateArray(x, y, value){
-        boardData[x][y] = value;
-    }
-    
-    function select(ele) {
-        document.querySelector(".scoreCard").style.display = "none";
-        if(ele.innerText == 'X'|| ele.innerText == 'O'){
-        }
-        else{
-        document.getElementById(ele.id).innerHTML = currentPlayer.value;        //Add current player value to element
-        if(currentPlayer.value == 'X'){
+}
+
+/**
+    This method is called when a player selects an empty square on a baord. The primarly goal of this method is to update the board and table to include the next character selected. But it also makes calls to check if the player has won and either updates the score or changes the player.
+**/
+function select(ele) {
+    document.querySelector(".scoreCard").style.display = "none";
+    if (ele.innerText == 'X' || ele.innerText == 'O') {} else {
+        document.getElementById(ele.id).innerHTML = currentPlayer.value; //Add current player value to element
+        if (currentPlayer.value == 'X') {
             document.getElementById(ele.id).style.color = '#8dd14d'; //Was #ee5a3b
-        }
-        else if(currentPlayer.value == 'O'){
+        } else if (currentPlayer.value == 'O') {
             document.getElementById(ele.id).style.color = '#f3823d';
         }
         ele.getAttribute("onclick")
@@ -90,212 +88,192 @@
         document.getElementById(ele.id).disabled = 'true';
         checkWin();
         updateScore();
-        if(playerType == 1){
-            togglePlayerAI();
-        }
-        else{
-            togglePlayer();
-        }
-        }
-  }
-        
-    function togglePlayer(){
-        if(currentPlayer == "undefined"){
-            currentPlayer == player1;
-        }
-        else if(currentPlayer == player1){
-            currentPlayer = player2;
-        }
-        else{
-            currentPlayer = player1;
-        }
-        document.getElementById("instructions").style.fontWeight = "0";
-        document.getElementById("instructions").innerHTML= currentPlayer.name+"("+currentPlayer.value+"): it's your turn";
+        togglePlayer();
     }
-    
-    function togglePlayerAI(){
-        if(currentPlayer == "undefined"){
-            currentPlayer == player1;
-        }
-        else if(currentPlayer == player1){
-            currentPlayer = playerAI;
-            selectAI();
-        }
-        else{
-            currentPlayer = player1;
-        }
-        document.getElementById("instructions").style.fontWeight = "0";
-        document.getElementById("instructions").innerHTML= currentPlayer.name+"("+currentPlayer.value+"): it's your turn";
-    }
-    
-    function selectAI(){
-        var i = 0;
-        var x;
-        var y;
-        
-        while(i == 0){
-            var a = Math.floor((Math.random() * boardWidth));
-            var b = Math.floor((Math.random() * boardWidth));
-            if(boardData[a][b] == undefined){
-                x = a;
-                y = b;
-                i = 1;
-            }
-            else{
-            }
-        }
-            var id = x+","+y;
-            var selectedButton = document.getElementById(id);
-            select(selectedButton);
-        }
-    
-    function resetPage(){
-        location.reload();
-    }
-    
-    function resetGame(){
-        for(i=0; i<boardData.length; i++){
-            for(j=0; j<boardData.length; j++){
-                boardData[i][j] = undefined;
-            }
-        }
-        updateGameType();
-    }
+}
 
+/**
+    This method is used to update a value in the array. It takes in the coordinates of the element and adds that's value to the array.
+**/
+function updateArray(x, y, value) {
+    boardData[x][y] = value;
+}
 
-    
-    function checkWin(){
-        var winSize;
-        if(boardWidth == 3){
-            winSize = 3;
-        }
-        else{
-            winSize = 5;
-        }
-        for(i=0; i<boardData.length; i++){
-            for(j=0; j<boardData.length; j++){
-                checkRowWin(i,j,winSize); //Works for both
-                checkColWin(i,j,winSize); //Works for both
-                checkRightUpDiaWin(i,j,winSize); //Works for both
-                checkLeftUpDiaWin(i,j,winSize); //Works for both
-            }
-        }
-        checkDraw();
+/**
+    This method's purpose is to change the currentPlayer variable to either player 1 or player 2. This method also updates the text element which prompts the players whose turn it is.
+**/
+function togglePlayer() {
+    if (currentPlayer == "undefined") {
+        currentPlayer == player1;
+    } else if (currentPlayer == player1) {
+        currentPlayer = player2;
+    } else {
+        currentPlayer = player1;
     }
-    
-    function checkRowWin(x,y,ws){
-        var count = 0;
-        var i;
-        for(i = 0; i <= ws-1; i++){
-        if(boardData[x][y+i] == currentPlayer.value){
+    document.getElementById("instructions").style.fontWeight = "0";
+    document.getElementById("instructions").innerHTML = currentPlayer.name + "(" + currentPlayer.value + "): it's your turn";
+}
+
+/**
+    This method is used to check whether a player of the game has won or draw. This method in particular, checks the boardsize, therefore determining if the win size is equal to 3 or 5. Then it uses a for loop to iterate through all of the square on the board and checks the horizontal, vertical and diagonal lines to check a win. It also makes a call to the check draw method.
+**/
+function checkWin() {
+    var winSize;
+    if (boardWidth == 3) {
+        winSize = 3;
+    } else {
+        winSize = 5;
+    }
+    for (i = 0; i < boardData.length; i++) {
+        for (j = 0; j < boardData.length; j++) {
+            checkRowWin(i, j, winSize); //Works for both
+            checkColWin(i, j, winSize); //Works for both
+            checkRightUpDiaWin(i, j, winSize); //Works for both
+            checkLeftUpDiaWin(i, j, winSize); //Works for both
+        }
+    }
+    checkDraw();
+}
+
+/**
+    This method takes in coordinates, from here these coordinates are used to check the horizontal elements next to it to determine if there are 3 or 5 of the same value in a line, thus winning a game; it then resets the game. Otherwise, it continues to play the game.
+**/
+function checkRowWin(x, y, ws) {
+    var count = 0;
+    var i;
+    for (i = 0; i <= ws - 1; i++) {
+        if (boardData[x][y + i] == currentPlayer.value) {
             count++;
-            }
-        }
-        if(count == ws){
-            declareWinner(currentPlayer.name + ' wins!');
-            currentPlayer.score++;
-            updateScore();
-            resetGame();
         }
     }
-    
-    function checkColWin(x,y,ws){
-        var count = 0;
-        var i;
-        for(i = 0; i <= ws-1; i++){
-            var a = x+i;
-            if(boardWidth <= a){
-            }
-            else{
-                if(boardData[x+i][y] == currentPlayer.value){
-                    count++;
-                }
-                else{
-                }
-            }
-        }
-        if(count == ws){
-            declareWinner(currentPlayer.name + ' wins!');
-            currentPlayer.score++;
-            updateScore();
-            resetGame();
-            
-        }
+    if (count == ws) {
+        declareWinner(currentPlayer.name + ' wins!');
+        currentPlayer.score++;
+        updateScore();
+        resetGame();
     }
-    
-    function checkRightUpDiaWin(x,y,ws){
-        var count = 0;
-        var i;
-        for(i = 0; i <= ws-1; i++){
-            var a = x+i;
-            if(boardWidth <= a){
-            }
-            else{
-                if(boardData[x+i][y+i] == currentPlayer.value){
-                    count++;
-                }
-                else{
-                }
-            }
-        }
-        if(count == ws){
-            declareWinner(currentPlayer.name + ' wins!');
-            currentPlayer.score++;
-            updateScore();
-            resetGame();
-        }
-    }
-    
-    function checkLeftUpDiaWin(x,y,ws){
-        var count = 0;
-        var i;
-        for(i = 0; i <= ws-1; i++){
-            var a = x+i;
-            if(boardWidth <= a){
-            }
-            else{
-                if(boardData[x+i][y-i] == currentPlayer.value){
-                    count++;
-                }
-                else{
-                }
-            }
-        }
-        if(count == ws){
-            declareWinner(currentPlayer.name + ' wins!');
-            currentPlayer.score++;
-            updateScore();
-            resetGame();
-            
-        }
-    }
+}
 
-function checkDraw(){
+/**
+    This method takes in coordinates, from here these coordinates are used to check the vertical elements below it to determine if there are 3 or 5 of the same value in a line, thus winning a game, it then resets the game. Otherwise, it continues to play the game.
+**/
+function checkColWin(x, y, ws) {
+    var count = 0;
+    var i;
+    for (i = 0; i <= ws - 1; i++) {
+        var a = x + i;
+        if (boardWidth <= a) {} else {
+            if (boardData[x + i][y] == currentPlayer.value) {
+                count++;
+            } else {}
+        }
+    }
+    if (count == ws) {
+        declareWinner(currentPlayer.name + ' wins!');
+        currentPlayer.score++;
+        updateScore();
+        resetGame();
+
+    }
+}
+
+/**
+    This method takes in coordinates, from here these coordinates are used to check the diagonal right upwards elements to determine if there are 3 or 5 of the same value in a line, thus winning a game, it then resets the game. Otherwise, it continues to play the game.
+**/
+function checkRightUpDiaWin(x, y, ws) {
+    var count = 0;
+    var i;
+    for (i = 0; i <= ws - 1; i++) {
+        var a = x + i;
+        if (boardWidth <= a) {} else {
+            if (boardData[x + i][y + i] == currentPlayer.value) {
+                count++;
+            } else {}
+        }
+    }
+    if (count == ws) {
+        declareWinner(currentPlayer.name + ' wins!');
+        currentPlayer.score++;
+        updateScore();
+        resetGame();
+    }
+}
+
+/**
+    This method takes in coordinates, from here these coordinates are used to check the diagonal left upwards elements to determine if there are 3 or 5 of the same value in a line, thus winning a game, it then resets the game. Otherwise, it continues to play the game.
+**/
+function checkLeftUpDiaWin(x, y, ws) {
+    var count = 0;
+    var i;
+    for (i = 0; i <= ws - 1; i++) {
+        var a = x + i;
+        if (boardWidth <= a) {} else {
+            if (boardData[x + i][y - i] == currentPlayer.value) {
+                count++;
+            } else {}
+        }
+    }
+    if (count == ws) {
+        declareWinner(currentPlayer.name + ' wins!');
+        currentPlayer.score++;
+        updateScore();
+        resetGame();
+
+    }
+}
+
+/**
+    This method checks the avaiable number of squares in the array. If there are no free squares the draws number is incremented, the scores are updated and a message is shown saying that there is a draw.
+**/
+function checkDraw() {
     var numOfAvalible = 0;
-    for(i=0; i<boardData.length; i++){
-            for(j=0; j<boardData.length; j++){
-                if(boardData[i][j] == null){
-                    numOfAvalible++;
-                }
+    for (i = 0; i < boardData.length; i++) {
+        for (j = 0; j < boardData.length; j++) {
+            if (boardData[i][j] == null) {
+                numOfAvalible++;
             }
-            }
-    if(numOfAvalible == 0){
+        }
+    }
+    if (numOfAvalible == 0) {
         drawsScore++;
         updateScore();
         declareWinner('Tie game!');
     }
 }
 
-function declareWinner(text){
+/**
+    This method takes in a parameter which is printed in a stylish square on the website, thus prompting whether a player has won, lost or drawn.
+**/
+function declareWinner(text) {
     document.querySelector(".scoreCard").style.display = "block";
-	document.querySelector(".scoreCard .text").innerText = text;
+    document.querySelector(".scoreCard .text").innerText = text;
 }
-    
-    function updateScore(){
-        if(playerType == 1){
-            document.getElementById("scores" ).innerHTML= "Scores: "+ player1.name + ': ' + player1.score + "  Draws: " + drawsScore + " " + playerAI.name + ": " + playerAI.score;
-        }
-        else{
-        document.getElementById("scores" ).innerHTML= "Scores: "+ player1.name + ': ' + player1.score + "  Draws: " + drawsScore + " " + player2.name + ": " + player2.score;
+
+/**
+    This method updates an element on the website which indicates to the users what the current scores are of the games played.
+**/
+function updateScore() {
+        document.getElementById("scores").innerHTML = "Scores: " + player1.name + ': ' + player1.score + "  Draws: " + drawsScore + " " + player2.name + ": " + player2.score;
+}
+
+/**
+    This method deletes the rows of the table; this is neccesary when resetting the game, otherwise more rows will be added to the bottom and the current table will always stay on the website.
+**/
+function deleteRows() {
+    var Parent = document.getElementById("gameTable");
+    while (Parent.hasChildNodes()) {
+        Parent.removeChild(Parent.firstChild);
+    }
+}
+
+/**
+    This method resets all the elements in the array to be empty; thus creating a whole new game.
+**/
+function resetGame() {
+    for (i = 0; i < boardData.length; i++) {
+        for (j = 0; j < boardData.length; j++) {
+            boardData[i][j] = undefined;
         }
     }
+    constructorFunc();
+}
